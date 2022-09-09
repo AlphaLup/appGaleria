b5d9b8902825c61618d37869517cb54d411389ce
Breno <brenoaaffonso@gmail.com>
2022-09-09 12:11:11 -0300

    Atividade 1 parte 2

--- app/src/main/java/amancio/breno/galeria/MainActivity.java
+++ app/src/main/java/amancio/breno/galeria/MainActivity.java
@@ -1,8 +1,13 @@
 package amancio.breno.galeria;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Define a toolbar para ser a actionbar padrão
+        Toolbar toolbar_main = findViewById(R.id.tbMain);
+        setSupportActionBar(toolbar_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Cria as opções de menudefinidas no arquivo de menu setado
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_tb, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // pega o id de um item e vai conferir se é igual ao da camera ou não
+        switch (item.getItemId()) {
+            case R.id.opCamera:
+                //caso seja, chamará o método definido
+                dispatchTakePictureIntent();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
--- app/src/main/java/amancio/breno/galeria/PhotoActivity.java
+++ app/src/main/java/amancio/breno/galeria/PhotoActivity.java
@@ -1,8 +1,14 @@
 package amancio.breno.galeria;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class PhotoActivity extends AppCompatActivity {
 
@@ -10,5 +16,37 @@ public class PhotoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
+
+        //Define a toolbar para ser a actionbar padrão
+        Toolbar toolbar_photo = findViewById(R.id.tbPhoto);
+        setSupportActionBar(toolbar_photo);
+
+        //Adiciona uma seta para voltar a uma activity pai definida no AndroidManifest
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Cria as opções de menudefinidas no arquivo de menu setado
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_tb, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //verifica se o id é igual ao compartilhar
+        switch (item.getItemId()) {
+            // caso for, chama o método definido
+            case R.id.opShare:
+                sharePhoto();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
